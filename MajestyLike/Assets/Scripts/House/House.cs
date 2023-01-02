984dbd28bdc5ee3323b930700611504d9bd0e613
Oleg Dvoretskiy <slay_wiltons@mail.ru>
2023-01-02 11:56:13 +0300

    show building resource info

--- MajestyLike/Assets/Scripts/House/House.cs
+++ MajestyLike/Assets/Scripts/House/House.cs
@@ -5,6 +5,7 @@ public class House : MonoBehaviour
 {
     [SerializeField] private int goldToAdd;
     [SerializeField] private float timeBeforeAdd;
+    [SerializeField] private ShowInfo testInfo;
     private int currentGold;
 
     private void Start()
@@ -17,5 +18,11 @@ public class House : MonoBehaviour
     {
         currentGold = ResourceCalculator.ResPlus(currentGold, goldToAdd);
         Debug.Log(currentGold);
+        testInfo.UpdateInfoText();
+    }
+
+    public int GetCurrentGold()
+    {
+        return currentGold;
     }
 }
--- MajestyLike/Assets/Scripts/House/ShowInfo.cs
+++ MajestyLike/Assets/Scripts/House/ShowInfo.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ShowInfo : MonoBehaviour
 {
+    [SerializeField] private House house;
+    [SerializeField] private GameObject testInfoPlate;
+    [SerializeField] private TextMeshProUGUI testText;
+
     private void OnMouseDown()
     {
-        
+        testInfoPlate.SetActive(true);
+        UpdateInfoText();
+    }
+
+    public void UpdateInfoText()
+    {
+        if (testInfoPlate.activeSelf)
+        {
+            testText.text = "Gold " + house.GetCurrentGold();
+        }
     }
 }
