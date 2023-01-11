b803fe4c9f75fbe372b7a8da6deae2d64e26620a
Oleg Dvoretskiy <slay_wiltons@mail.ru>
2023-01-11 23:54:23 +0300

    Added character base

---
+++ MajestyLike/Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New base character", menuName = "Base character")]
+public class BaseCharacter : ScriptableObject
+{
+    public string FName;
+    public string SName;
+}
