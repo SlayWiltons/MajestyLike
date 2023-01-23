a766e951846326b627175911b7d33d5f987cb85d
Oleg Dvoretskiy <slay_wiltons@mail.ru>
2023-01-23 21:46:43 +0300

    Added interfaces, improved character

--- MajestyLike/Assets/Scripts/Characters/Character.cs
+++ MajestyLike/Assets/Scripts/Characters/Character.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class Character : MonoBehaviour, IDamageable<int>, IKillable
 {
     [SerializeField] private string name;
     [SerializeField] private Char_Class charClass;
     [SerializeField] private GameObject charModel;
+
+    public void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage(int takenDamage)
+    {
+        throw new System.NotImplementedException();
+    }
 }
